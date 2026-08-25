c8a929d15e8beb4621382e6839bf91af99c0b6c6
LiZhenYu <2383433922@qq.com>
2026-08-25 18:03:59 +0800

    预制体

---
+++ ReDiv_Online/Assets/Scripts/Game/Scripts/Town/TownCharacterController.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using XFramework;
+
+public class TownCharacterController : GameBase
+{
+    private Transform SkeletonTran;
+    private TextMeshPro NameText;
+
+}
